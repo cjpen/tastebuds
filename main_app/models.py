c10857dd1221a1be95a580580987f3b296ba864d
cjpen <cjpen@me.com>
2022-02-12 12:45:15 -0800

    Add Group User model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 from django.urls import reverse
-from datetime import date
+from datetime import date, datetime
 # Import the User
 from django.contrib.auth.models import User
+
+class Group(models.Model):
+    name=models.CharField(max_length=50)
+    description=models.TextField(max_length=250)
+    leader_username = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    members = models.ManyToManyField(User)
+
+    def __str__(self):
+        return f'{self.name} ({self.id})'
+
 
