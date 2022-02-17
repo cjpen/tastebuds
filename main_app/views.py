6c04f3e5c26bc348bd8e7c4a5af82fd74cb2acf3
adevlinb <devlinbgenassembly@gmail.com>
2022-02-16 19:09:33 -0800

    adds photos

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,6 @@
+import os
+import boto3
+import uuid
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth import login
@@ -6,7 +9,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import models
 from django.template.defaultfilters import register
-from .models import Group, Event, Profile, User, Recipe, Vote
+from .models import Group, Event, Profile, User, Recipe, Vote, PhotoGroup, PhotoEvent, PhotoRecipe
 from .forms import EventForm, ProfileForm, RecipeForm
 
 # Custom Filter
@@ -14,11 +17,11 @@ from .forms import EventForm, ProfileForm, RecipeForm
 def get_item(dictionary, key):
   return dictionary.get(key)
 
-# Create your views here.
 
 class GroupUpdate(LoginRequiredMixin, UpdateView):
   model = Group
   fields = ['name', 'description']
+
 
 class GroupDelete(LoginRequiredMixin, DeleteView):
   model = Group
@@ -28,26 +31,30 @@ class GroupDelete(LoginRequiredMixin, DeleteView):
 def home(request):
   return render(request, 'home.html')
 
+@login_required
 def dashboard (request):
   profile = Profile.objects.get(user=request.user)
   groups = Group.objects.filter(members=profile)
   return render(request, 'dashboard.html', {'groups': groups})
 
+@login_required
 def groups_index(request):
   groups = Group.objects.all()
   return render(request, 'groups/index.html', {'groups': groups})
 
+@login_required
 def groups_detail(request, group_id):
   group = Group.objects.get(id=group_id)
   # events = Event.objects.get(id=group_id)
   event_form = EventForm()
   return render(request, 'groups/detail.html',{'group': group, 'event_form': event_form})
 
+@login_required
 def events_detail(request, event_id):
   event = Event.objects.get(id=event_id)
 
   recipes_in_event = Recipe.objects.filter(events=event_id)
-  recipes_not_in_event = Recipe.objects.exclude(events=event_id)
+  recipes_not_in_event = Recipe.objects.exclude(events=event_id, user=request.user)
 
   votes = Vote.objects.filter(event_id=event_id).values('recipe_id').annotate(cnt=models.Count('recipe_id'))
   tally = {}
@@ -61,6 +68,7 @@ def events_detail(request, event_id):
     'tally': tally,
   })
 
+@login_required
 def add_event(request, group_id):
   #create a ModelForm instance using the data in request.POST
   form = EventForm(request.POST)
@@ -73,6 +81,7 @@ def add_event(request, group_id):
     new_event.save()
   return redirect('detail', group_id=group_id)
 
+@login_required
 def events_create(request, group_id):
   event_form = EventForm()
   return render(request, {'event_form': event_form}, group_id=group_id)
@@ -92,10 +101,12 @@ def signup(request):
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
 
+
 def profile_form(request):
   profile_form = ProfileForm()
   return render(request, 'registration/profile_form.html', {'profile_form': profile_form})
 
+@login_required
 def add_profile(request):
   current_user = request.user
   #create a ModelForm instance using the data in request.POST
@@ -107,7 +118,7 @@ def add_profile(request):
     new_profile.save()
   return redirect('dashboard')
 
-# Class-Based View (CBV)
+
 class GroupCreate(LoginRequiredMixin, CreateView):
   model = Group
   fields = ['name', 'description']
@@ -120,6 +131,7 @@ class GroupCreate(LoginRequiredMixin, CreateView):
     form.instance.leader = self.request.user.username
     return super().form_valid(form)
 
+@login_required
 def add_event(request, group_id):
   #create a ModelForm instance using the data in request.POST
   form = EventForm(request.POST)
@@ -132,6 +144,7 @@ def add_event(request, group_id):
     new_event.save()
   return redirect('detail', group_id=group_id)
 
+@login_required
 def events_create(request, group_id):
   event_form = EventForm()
   return render(request, {'event_form': event_form}, group_id=group_id)
@@ -153,6 +166,7 @@ def unassoc_profile(request, group_id):
   group.members.remove(profile_id)
   return redirect('detail', group_id=group_id)
 
+@login_required
 def recipes_index(request):
   recipe_form = RecipeForm()
   recipes = Recipe.objects.filter(user=request.user)
@@ -161,9 +175,9 @@ def recipes_index(request):
     'recipes': recipes
   })
 
+@login_required
 def add_recipe(request):
   form = RecipeForm(request.POST)
-  print('adele says hello')
   if form.is_valid():
     new_recipe = form.save(commit=False)
     new_recipe.user = request.user
@@ -186,3 +200,83 @@ def vote_up(request, event_id, recipe_id):
   else:
     Vote.objects.create(event_id=event_id, recipe_id=recipe_id, user=request.user)
     return redirect('events_detail', event_id)
+
+@login_required
+def add_photo_group(request, group_id):
+  # photo file will be the "name" attribute of the input
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+    s3 = boto3.client('s3')
+    # need a unique "key" name for s3
+    # and need the same file extension as well
+    key = uuid.uuid4().hex[:8] + photo_file.name[photo_file.name.rfind('.'):]
+    try:
+      bucket = os.environ['S3_BUCKET']
+      s3.upload_fileobj(photo_file, bucket, key)
+      url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
+      PhotoGroup.objects.create(url=url, group_id=group_id)
+    except Exception as e:
+      print('An error occured uploading file to S3')
+      print(e)
+
+  return redirect('detail', group_id=group_id)
+
+@login_required
+def add_photo_event(request, event_id):
+  # photo file will be the "name" attribute of the input
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+    s3 = boto3.client('s3')
+    # need a unique "key" name for s3
+    # and need the same file extension as well
+    key = uuid.uuid4().hex[:8] + photo_file.name[photo_file.name.rfind('.'):]
+    try:
+      bucket = os.environ['S3_BUCKET']
+      s3.upload_fileobj(photo_file, bucket, key)
+      url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
+      PhotoEvent.objects.create(url=url, event_id=event_id)
+    except Exception as e:
+      print('An error occured uploading file to S3')
+      print(e)
+  return redirect('events_detail', event_id=event_id)
+
+# @login_required
+# def add_photo_profile(request, profile_id):
+#   # photo file will be the "name" attribute of the input
+#   photo_file = request.FILES.get('photo-file', None)
+#   if photo_file:
+#     s3 = boto3.client('s3')
+#     # need a unique "key" name for s3
+#     # and need the same file extension as well
+#     key = uuid.uuid4().hex[:8] + photo_file.name[photo_file.name.rfind('.'):]
+#     try:
+#       bucket = os.environ['S3_BUCKET']
+#       s3.upload_fileobj(photo_file, bucket, key)
+#       url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
+#       Photo.objects.create(url=url, cat_id=cat_id)
+#     except Exception as e:
+#       print('An error occured uploading file to S3')
+#       print(e)
+
+#   return redirect('detail', profile_id=profile_id)
+
+@login_required
+def add_photo_recipe(request, recipe_id):
+  # photo file will be the "name" attribute of the input
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+    s3 = boto3.client('s3')
+    # need a unique "key" name for s3
+    # and need the same file extension as well
+    key = uuid.uuid4().hex[:8] + photo_file.name[photo_file.name.rfind('.'):]
+    try:
+      bucket = os.environ['S3_BUCKET']
+      s3.upload_fileobj(photo_file, bucket, key)
+      url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
+      PhotoRecipe.objects.create(url=url, recipe_id=recipe_id)
+    except Exception as e:
+      print('An error occured uploading file to S3')
+      print(e)
+
+  return redirect('recipes_index')
+
